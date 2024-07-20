4d8acf8e7c3118cbac2dcca14481b5487b620f1a
bijiaxing <15734069790@163.com>
2024-07-20 14:05:49 +0800

    add category description

--- application/api/controller/Survey.php
+++ application/api/controller/Survey.php
@@ -15,11 +15,8 @@
 namespace app\api\controller;
 
 use app\common\controller\Api;
-use app\common\library\Ems as Emslib;
-use app\member\model\Member;
-use think\facade\Hook;
-use think\facade\Validate;
-
+use app\admin\model\User;
+use think\Db;
 /**
  * @title 邮箱验证码接口
  * @controller api\controller\Ems
@@ -54,6 +51,41 @@ class Survey extends Api
         $survey['categories'] = $categories;
         $survey['web_site_survey_result_display'] =  config('site.web_site_survey_result_display');
         $this->success('发送成功', $survey);
+    }
+
+    /**
+     * 提交问卷
+     */
+    public function submit(){
+        //判断提交方式，只允许post提交
+        if (!$this->request->isPost()) {
+            $this->error('非法请求');
+        }
+        //获取参数
+        $data = $this->request->post();
+        $uuid = $data['uuid'];
+        //获取问卷
+        $survey = \app\admin\model\Survey::where('uuid', $uuid)->find();
+        if (!$survey) {
+            $this->error('问卷不存在');
+        }
+        DB::startTrans();
+        //添加用户信息到user表
+        $user = new User();
+        $userData = [
+            'sid' => $data['sid'],
+            'user_name' => $data['user_name'] ?? '',
+            'user_id' => $data['user_id'] ?? '',
+            'sex' => $data['sex'] ?? '',
+            'department' => $data['department'] ?? '',
+            'birth' => $data['birth'] ?? '',
+            'result_score' => $data['result_score'] ?? '',
+            'result_text' => $data['result_text'] ?? '',
+        ];
+
+        $user->save($userData);
+
+        
     }
 
 
