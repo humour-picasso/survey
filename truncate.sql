4d772e56b2e08223d44c6520282c96c1ecc14f19
connbi <conn@ciandt.com>
2024-07-25 18:12:32 +0800

    add sql

---
+++ truncate.sql
@@ -0,0 +1,8 @@
+TRUNCATE TABLE yzn_category_score;
+TRUNCATE TABLE yzn_category;
+TRUNCATE TABLE yzn_survey;
+TRUNCATE TABLE yzn_user;
+TRUNCATE TABLE yzn_user_result;
+TRUNCATE TABLE yzn_user_answer;
+TRUNCATE TABLE yzn_question;
+TRUNCATE TABLE yzn_options;
