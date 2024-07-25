7b7bb1a5356fa5ccac31a1422ea41a776a2924c2
connbi <conn@ciandt.com>
2024-07-25 18:37:52 +0800

    update

--- application/admin/controller/Survey.php
+++ application/admin/controller/Survey.php
@@ -166,14 +166,13 @@ class Survey extends Adminbase
                     $params['categories'] = !empty($params['categories']) ? implode(',', $params['categories']) : $params['categories'][0];
                     $result = $row->allowField(true)->save($params);
                     //添加关系
-                    $surveyId = $this->modelClass->id;
                     if (!empty($params['categories'])) {
                         $categories = explode(',', $params['categories']);
+                        //先删除
+                        Db::name('survey_category_relationship')->where('sid', $id)->delete();
                         foreach ($categories as $category) {
-                            //先删除
-                            Db::name('survey_category_relationship')->where('sid', $surveyId)->delete();
                             $data = [
-                                'sid' => $surveyId,
+                                'sid' => $id,
                                 'cid' => $category
                             ];
                             Db::name('survey_category_relationship')->insert($data);
@@ -225,52 +224,33 @@ class Survey extends Adminbase
         $this->request->filter(['strip_tags', 'trim']);
         if ($this->request->isAjax()) {
             //如果发送的来源是Selectpage，则转发到Selectpage
-            $params = $this->request->request();
-            $page = $params['page'];
-            $limit = $params['limit'];
-            $filter = json_decode($params['filter'], true);
-            if (!empty($filter['survey_name'])) {
-                $where['survey_name'] = ['like', '%'.$filter['survey_name'].'%'];
-                $list = $this->modelClass->where($where)->select();
-            } else {
-                $list = $this->modelClass->select();
+            if ($this->request->request('keyField')) {
+                return $this->selectpage();
             }
-            $result = [];
-            foreach ($list as $key =>$row) {
-                
-                $list[$key]['categories'] = explode(',', $row['categories']);
-                $categories = [];
-                if (!empty($list[$key]['categories'])) {
-                    foreach ($list[$key]['categories'] as $k => $v) {
-                        if (!empty($filter['category_name'])) {
-                            $cwhere['category_name'] = ['like', '%'.$filter['category_name'].'%'];
-                        }  else {
-                            $cwhere = ['1' => '1'];
-                        }
-                        $categories[$k] = Category::where('id', $v)->where($cwhere)->value('name');
-                        $categoriesScore = Score::where('cid', $v)->select();
-                        if (!empty($categoriesScore)) {
-                            foreach ($categoriesScore as $kk => $vv) {
-                                $categoriesScore[$kk]['count'] = Db::name('user_result ur')
-                                ->leftjoin('user u', 'u.id = ur.uid')
-                                ->where('score', '>=', $vv['score_start'])
-                                ->where('score', '<=', $vv['score_end'])
-                                ->where('cid', $v)
-                                ->where('sid', $row['id'])
-                                ->count();
-                                $result[] = [
-                                    'survey_name' => $row['survey_name'], 
-                                    'category_name' => $categories[$k], 
-                                    'score_limit' => $vv['score_start'] . ' - ' . $vv['score_end'],
-                                    'count' => $categoriesScore[$kk]['count']
-                                ];
-                            }
-                        }
-                    }
-                }
-            }
+        
+            [$page, $limit, $where, $sort, $order] = $this->buildTableParames();
             
-            $result = ["code" => 0, "count" => count($result), "data" => $result];
+            $list = $this->modelClass
+            ->leftJoin('survey_category_relationship scr', 'scr.sid = yzn_survey.id')
+            ->leftJoin('category c', 'c.id = scr.cid')
+            ->leftJoin('category_score cs', 'c.id = cs.cid')
+            ->field('yzn_survey.survey_name, scr.sid, scr.cid, c.name, cs.score_start, cs.score_end, cs.result')
+                    ->where($where)
+                    ->order('yzn_survey.id', $order)
+                    ->paginate($limit);
+            //统计人数
+            foreach ($list as $key=>$row) {
+                $list[$key]['userCount'] = Db::name('user_result')
+                ->leftjoin('user u', 'u.id = uid')
+                ->where('score', '>=', $row['score_start'])
+                ->where('score', '<=', $row['score_end'])
+                ->where('cid', $row['cid'])
+                ->where('sid', $row['sid'])
+                ->count();
+                
+                $list[$key]['score_limit'] = $row['score_start'] . ' - ' . $row['score_end'];
+            }
+            $result = ["code" => 0, "count" => $list->total(), "data" => $list->items()];
 
             return json($result);
         }
